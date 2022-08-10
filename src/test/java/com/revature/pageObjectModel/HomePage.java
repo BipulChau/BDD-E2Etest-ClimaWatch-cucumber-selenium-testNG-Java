b8fc0077b2b067b5e85c915f48d7897ef2584ed8
Bipul Chaudhary <bipulchau@gmail.com>
2022-08-10 13:50:54 -0400

    getter methods to get the webelements

--- src/test/java/com/revature/pageObjectModel/HomePage.java
+++ src/test/java/com/revature/pageObjectModel/HomePage.java
@@ -8,13 +8,13 @@ import org.openqa.selenium.support.PageFactory;
 public class HomePage {
     private WebDriver driver;
 
-    @FindBy(id="city")
+    @FindBy(id = "city")
     private WebElement enterCityName;
 
-    @FindBy(id="search_button")
-    private  WebElement searchBtn;
+    @FindBy(id = "search_button")
+    private WebElement searchBtn;
 
-    @FindBy(id="my_location")
+    @FindBy(id = "my_location")
     private WebElement use_current_location_btn;
 
     @FindBy(xpath = "//a[@id='join_now']")
@@ -26,44 +26,116 @@ public class HomePage {
     @FindBy(id = "user_name")
     private WebElement usernameSignInForm;
 
-    @FindBy(id="user_password")
+    @FindBy(id = "user_password")
     private WebElement passwordSignInForm;
 
-    @FindBy(id="signin_btn")
-    private WebElement SignInBtn;
+    @FindBy(id = "signin_btn")
+    private WebElement signInBtn;
 
-   @FindBy(id="username")
-   private WebElement usernameJoinNowForm;
+    @FindBy(id = "username")
+    private WebElement usernameJoinNowForm;
 
-   @FindBy(id="name")
+    @FindBy(id = "name")
     private WebElement nameJoinForm;
 
-   @FindBy(id="email")
+    @FindBy(id = "email")
     private WebElement emailJoinForm;
 
-   @FindBy(id="password")
+    @FindBy(id = "password")
     private WebElement passwordJoinForm;
 
-   @FindBy(id="registration_btn")
+    @FindBy(id = "registration_btn")
     private WebElement joinNowBtn;
 
-   @FindBy(id="user_info")
+    @FindBy(id = "user_info")
     private WebElement userInfoAfterLogin;
 
-   @FindBy(id="sign_out")
+    @FindBy(id = "sign_out")
     private WebElement signOutBtn;
 
-   @FindBy(id="table")
+    @FindBy(id = "table")
     private WebElement table;
 
-   @FindBy(id="weather_info")
+    @FindBy(id = "weather_info")
     private WebElement displayWeatherInfo;
 
     @FindBy(xpath = "//*[@id='add_to_fav_btn']")
     private WebElement addToFavBtn;
 
-public HomePage(WebDriver driver){
-    this.driver = driver;
-    PageFactory.initElements(driver, this);
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public WebElement getEnterCityName() {
+        return enterCityName;
+    }
+
+    public WebElement getSearchBtn() {
+        return searchBtn;
+    }
+
+    public WebElement getUse_current_location_btn() {
+        return use_current_location_btn;
+    }
+
+public WebElement getJoinNowTab(){
+        return  joinNowTab;
+}
+
+public WebElement getSignInTab(){
+        return signInTab;
+}
+
+public WebElement getUsernameSignInForm(){
+        return usernameSignInForm;
+}
+
+public WebElement getPasswordSignInForm(){
+        return passwordSignInForm;
+}
+
+public WebElement getSignInBtn(){
+        return signInBtn;
+}
+
+public WebElement getUsernameJoinNowForm(){
+        return usernameJoinNowForm;
+}
+
+public WebElement getNameJoinForm(){
+        return nameJoinForm;
+}
+
+public WebElement getEmailJoinForm(){
+        return emailJoinForm;
+}
+
+public  WebElement getPasswordJoinForm(){
+        return passwordJoinForm;
+}
+
+public WebElement getJoinNowBtn(){
+        return joinNowBtn;
+}
+
+public WebElement getUserInfoAfterLogin(){
+        return userInfoAfterLogin;
+}
+
+public  WebElement getSignOutBtn(){
+        return signOutBtn;
+}
+
+public  WebElement getTable(){
+        return table;
+}
+
+public  WebElement getDisplayWeatherInfo(){
+        return displayWeatherInfo;
+}
+
+public WebElement getAddToFavBtn(){
+        return addToFavBtn;
 }
 }
